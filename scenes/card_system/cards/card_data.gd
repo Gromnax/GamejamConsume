d776a7ff6ff612f69ef404c8df1a588f92e74e6a
Jack Kaller <jackson-kaller@proton.me>
2025-07-14 17:56:13 +0200

    weight int to float

--- scenes/card_system/cards/card_data.gd
+++ scenes/card_system/cards/card_data.gd
@@ -3,12 +3,12 @@ class_name CardData
 
 @export var keyword : String 
 ##Negative : Left, Zero : Neutral, Positive : Right
-@export var politics_weight : int
+@export var politics_weight : float
 var left_multiplier: float
 var right_multiplier: float
 	
 
-func _init(label: String = "Empty keyword", weight: int = 0, new_left_multiplier: float = 1, new_right_multiplier: float = 1) -> void:
+func _init(label: String = "Empty keyword", weight: float = 0, new_left_multiplier: float = 1, new_right_multiplier: float = 1) -> void:
 	self.keyword = label
 	self.politics_weight = weight
 	self.left_multiplier = new_left_multiplier
--- scenes/card_system/cards/keyword_weight.gd
+++ scenes/card_system/cards/keyword_weight.gd
@@ -2,11 +2,11 @@ extends Resource
 class_name KeywordWeight
 
 var label: String
-var weight: int
+var weight: float
 var left_multiplier: float
 var right_multiplier: float
 
-func _init(new_label: String, new_weight: int, new_left_multiplier: float = 1, new_right_multiplier: float = 1) -> void:
+func _init(new_label: String, new_weight: float, new_left_multiplier: float = 1, new_right_multiplier: float = 1) -> void:
 	self.label = new_label
 	self.weight = new_weight
 	self.left_multiplier = new_left_multiplier
